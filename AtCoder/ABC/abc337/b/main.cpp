35e1edb8ce262bb6e47fb6e5ad8f025358408221
VvyLw <owl1465fu9@gmail.com>
2024-01-20 23:49:31 +0900

    resolved

--- AtCoder/ABC/abc337/b/main.cpp
+++ AtCoder/ABC/abc337/b/main.cpp
@@ -2,6 +2,7 @@
 #pragma GCC optimize("unroll-loops")//*/
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include "C++/template.hpp"
+#include "C++/Strings.hpp"
 
 using namespace std;
 using namespace zia_qu;
@@ -20,23 +21,10 @@ int main() {
 
 // --------------------------------------------------------------------------------------------------------------
 
-string encode(const string& str) {
-    int n = (int)str.size();
-    string ret = ""; // 答えを格納
-    for (int l = 0; l < n;) {
-        int r = l + 1;
-        for (; r < n && str[l] == str[r]; r++) {};
-        ret.push_back(str[l]);
-        string num_str = to_string(r - l); // 出現回数
-        ret += num_str;
-        l = r;
-    }
-    return ret;
-}
 
 void VvyLw::solve() {
 	STR(s);
-	const auto t=encode(s);
+	const auto t=rle(s);
 	debug(t);
 	string al;
 	each(c,t) {
