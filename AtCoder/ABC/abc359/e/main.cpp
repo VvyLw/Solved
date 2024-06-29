f05f877342b8e9c4e2ed8e427f85a565f76da2ae
VvyLw <owl1465fu9@gmail.com>
2024-06-30 03:30:01 +0900

    solved

--- AtCoder/ABC/abc359/e/main.cpp
+++ AtCoder/ABC/abc359/e/main.cpp
@@ -2,6 +2,7 @@
 #pragma GCC optimize("unroll-loops")//*/
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include "C++/template.hpp"
+#include "C++/ds/SegmentTreeBeats.hpp"
 using namespace std;
 using namespace zia_qu;
 using namespace Lady_sANDy;
@@ -23,6 +24,12 @@ int main() {
 void VvyLw::solve() {
 	INT(n);
 	VEC(int,h,n);
-	
+	SegmentTreeBeats seg(n);
+	vi a;
+	rep(n) {
+		seg.chmax(0,i+1,h[i]);
+		a.emplace_back(seg.sum(0,i+1));
+	}
+	++a;
 	out(a);
 }
