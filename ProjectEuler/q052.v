6fbc6bba00ad0d5bb0d514ecb26eccb8c5970d5e
VvyLw <owl1465fu9@gmail.com>
2025-03-21 17:40:33 +0900

    a pe a day

---
+++ ProjectEuler/q052.v
@@ -0,0 +1,23 @@
+fn main() {
+	for x in 10000..100000000 {
+		mut cnt := []int{len: 10}
+		for c in x.str() {
+			cnt[c - '0'[0]]++
+		}
+		mut ok := true
+		for i in 2..7 {
+			mut d := []int{len: 10}
+			for c in (x * i).str() {
+				d[c - '0'[0]]++
+			}
+			ok &&= cnt == d
+		}
+		if ok {
+			println(x)
+			for i in 2..7 {
+				eprintln(x * i)
+			}
+			break
+		}
+	}
+}
